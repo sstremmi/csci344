a7ed71f4a5996cb1e111ebca4b5c678011db7ae8
Sumner <sstremmi@unca.edu>
2025-03-03 18:26:24 -0500

    tutorial 6 for real this time

--- tutorials/tutorial06/main.js
+++ tutorials/tutorial06/main.js
@@ -1,4 +1,3 @@
-// global variables tracking the user's preferences:
 let searchTerm = "";
 let openOnly = false;
 
@@ -8,51 +7,54 @@ const search = (ev) => {
     // Set user's preferences (global variables) from the DOM:
     searchTerm = document.querySelector("#search_term").value;
     openOnly = document.querySelector("#is_open").checked;
+    console.log(searchTerm, openOnly);
 
     // Invoke the show matching courses function
     showMatchingCourses();
 };
 
 // Part 1.1a
-const isClassFull = (course) => {
-    return course.EnrollmentMax <= course.EnrollmentCurrent;
+const isClassOpen = (course) => {
+    // if they're applying the "open only" filter:
+    return course.EnrollmentCurrent < course.EnrollmentMax;
 };
 
 // Part 1.1b
 const doesTermMatch = (course) => {
+    let match = false;
 
+    // title check
     if (course.Title.toLowerCase().includes(searchTerm.toLowerCase())) {
-        return true;
+        match = true;
     }
-    else {
-        return false;
-    }
+    return match;
 };
 
 // Part 1.2
 const dataToHTML = (course) => {
-
-    if (isClassFull(course)) {
-         status = '<i class="fa-solid fa-circle-xmark"></i> Closed'
+    // modify this to be more
+    let status;
+    if (!isClassOpen(course)) {
+        status = `<i class="fa-solid fa-circle-xmark"></i> Closed`;
     } else {
-         status = '<i class="fa-solid fa-circle-check"></i> Open'
+        status = `<i class="fa-solid fa-circle-check"></i> Open`;
     }
-
-const seatsAvailable = course.EnrollmentMax - course.EnrollmentCurrent;
-if (seatsAvailable < 0) {
-    seatsAvailable = 0;
-}
-
-
+    let seatsAvailable = course.EnrollmentMax - course.EnrollmentCurrent;
+    if (seatsAvailable < 0) {
+        seatsAvailable = 0;
+    }
     return `
         <section class="course">
             <h2>${course.Code}: ${course.Title}</h2>
             <p>
-                ${status}
-                &bull; ${course.CRN} &bull; Seats Available: ${seatsAvailable}
+                ${status} &bull; ${
+        course.CRN
+    } &bull; Seats Available: ${seatsAvailable}
             </p>
             <p>
-                ${course.Days || "TBD"} &bull; ${course.Location.FullLocation || "TBD"} &bull; ${course.Hours} credit hour(s)
+                ${course.Days || "TBD"} &bull; ${
+        course.Location.FullLocation || "TBD"
+    } &bull; ${course.Hours}
             </p>
             <p><strong>${course.Instructors[0].Name}</strong></p>
         </section>
@@ -66,15 +68,20 @@ const showMatchingCourses = () => {
     console.log(`Course data:`, courseList);
 
     // output all of the matching courses to the screen:
-
     const container = document.querySelector(".courses");
-    container.innerHTML = "";
+    container.innerHTML = null;
 
+    // filter by search term:
     let matches = courseList.filter(doesTermMatch);
 
-    matches.forEach(course => {
+    // If they only want to see open courses, apply the filter:
+    if (openOnly) {
+        matches = matches.filter(isClassOpen);
+    }
+
+    matches.forEach((course) => {
+        // if the class is open, show it:
         const snippet = dataToHTML(course);
-        console.log(snippet);
         container.insertAdjacentHTML("beforeend", snippet);
-    })
-}
+    });
+};
